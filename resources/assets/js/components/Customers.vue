35008c0f6322f2e41deda8a77947c7e44187e184
Taron <taronsuvaryan18@gmail.com>
2018-07-15 10:03:07 +0400

    vue_js

---
+++ resources/assets/js/components/Customers.vue
@@ -0,0 +1,40 @@
+<template>
+    <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for = "customer in customers">
+                    <td>{{customer.name}}</td>
+                    <td>{{customer.email}}</td>
+                </tr>
+            </tbody>
+    </table>
+</template>
+
+<script>
+
+    export default {
+
+        data() {
+            return {
+                customers: [],
+            }
+        },
+
+
+
+        created () {
+            this.fetchCustomer();
+        },
+
+        methods:{
+            fetchCustomer(){
+                 this.$http.get('customer').then(response  => { this.customers = response.data.customers});
+            }
+        }
+    }
+</script>
